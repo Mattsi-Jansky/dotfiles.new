37c3f1fb900d32fdd57f37382037dac812816db2
Mattsi <mattsi@jansky.dev>
2026-03-30 11:40:31 +0100

    Add new dotfiles to step

--- steps/dotfiles.py
+++ steps/dotfiles.py
@@ -1,6 +1,9 @@
 from framework.providers.symlinks import symlink_dotfiles, Dotfile
 
 symlink_dotfiles("Dotfiles", [
-    Dotfile("home/.zshrc",            "~/.zshrc"),
-    Dotfile("home/.gitconfig",        "~/.gitconfig"),
+    Dotfile("home/.zshrc",      "~/.zshrc"),
+    Dotfile("home/.gitconfig",  "~/.gitconfig"),
+    Dotfile("home/.aliases",    "~/.aliases"),
+    Dotfile("home/.funcs",      "~/.funcs"),
+    Dotfile("home/.npmrc",      "~/.npmrc"),
 ])
